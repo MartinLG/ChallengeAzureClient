49c2ea9d612fc21b39ac236d1be3e55db222efd2
MartinLG <martin.leguillou@ynov.com>
2016-02-12 15:02:22 +0100

    redis strat

--- redis-child.js
+++ redis-child.js
@@ -17,6 +17,9 @@ var b36 = baseN.create({
 
 var range = b36.decode(end) - b36.decode(string);
 
+console.log('range =>')
+console.log(range);
+
 for (var i = 0; i < range; i++) {
 	string = b36.encode(b36.decode(string) + i);
 	var hash = sha1(string + salt);
